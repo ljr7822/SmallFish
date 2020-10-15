d61339724799b70032c4d840d0b22f5b376526f1
iwenLjr <1816807688@qq.com>
2020-10-16 02:03:28 +0800

    使用OkHttp3搭建并封装网络库请求

--- app/src/main/java/com/example/smallfish/MainActivity.java
+++ app/src/main/java/com/example/smallfish/MainActivity.java
@@ -12,6 +12,10 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.alibaba.fastjson.JSONObject;
+import com.example.libnetwork.ApiResponse;
+import com.example.libnetwork.GetRequest;
+import com.example.libnetwork.JsonCallBack;
 import com.example.smallfish.utils.NavGraphBuilder;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -23,15 +27,6 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        BottomNavigationView navView = findViewById(R.id.nav_view);
-//        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-//        NavigationUI.setupWithNavController(navView, navController);
-//
-//        NavGraphBuilder.build(navController);
-//
-//        // 关联页面和点击事件
-//        navView.setOnNavigationItemSelectedListener(this);
-
         BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
@@ -44,6 +39,16 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         NavGraphBuilder.build(navController,this,fragment.getId());
 
         navView.setOnNavigationItemSelectedListener(this);
+
+        GetRequest<JSONObject> request = new GetRequest<JSONObject>("www.mooc.com");
+        request.execute();
+
+        request.execute(new JsonCallBack<JSONObject>() {
+            @Override
+            public void onSuccess(ApiResponse<JSONObject> response) {
+                super.onSuccess(response);
+            }
+        });
     }
 
     @Override
